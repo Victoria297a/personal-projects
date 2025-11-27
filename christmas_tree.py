b493873ebb6c52092e7b27feff9a63529152bd3f
Victoria297a <vikkiv2k18@gmail.com>
2025-11-27 15:20:22 +0100

    added christmas tree

---
+++ christmas_tree.py
@@ -0,0 +1,46 @@
+import sys
+import time
+import os
+import random
+from turtle import color
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def print_tree(lights):
+    tree = [
+        "        *        ",
+        "       ***       ",
+        "      *****     ",
+        "     *******     ",
+        "    *********    ",
+        "   ***********   ",
+        "       |||       "
+    ]
+    
+    colors = {
+        'green': '\033[92m',
+        'yellow': '\033[93m',
+        'red': '\033[91m',
+        'brown': '\033[33m',
+        'reset': '\033[0m'
+    }
+
+    for line in tree:
+        for char in line:
+            if char == '*':
+                color = random.choice(list(colors.values()))
+                print(color + char, end="")
+            elif char == '|':
+                print(colors['brown'] + char, end="")
+            else:
+                print(" ", end="")
+        print(colors['reset'])
+def main():
+    while True:
+        clear_console()
+        print_tree(6)
+        time.sleep(0.5)
+        
+        
+if __name__ == "__main__":
+    main()
